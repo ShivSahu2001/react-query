3eb4804573f77dc68e84b84fee71550af18b8bfc
Shivshankar Sahu <shivsahu495@gmail.com>
2023-06-10 17:14:20 +0530

    useMutation hook to post data

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,30 +1,48 @@
 
-// import { useState } from "react";
+import { useState } from "react";
 
-import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
+import { useAddSuperHeroData, useSuperHeroesData } from "../hooks/useSuperHeroesData";
 import {Link} from "react-router-dom"
 
 
 const RQSuperHeroesPage = () => {
 
     // const [interval, setInterval] = useState(5000);
+    const [name, setName] = useState('');
+    const [alterEgo, setAlterEgo] = useState('');
 
     const onSuccess = (data) => {
         // if (data.data.length >=4 && interval === 3000) {
         //     setInterval(false)
         // }
-        // console.log("Perform side effects after data fetching like navigating to another page or opening a modal", data)
+        console.log("Perform side effects after data fetching like navigating to another page or opening a modal", data)
     }
     const onError = (error) => {
         // setInterval(false)
-        // console.log("Perform side effects after encountering error", error)
+        console.log("Perform side effects after encountering error", error)
     }
 
   // useQuery(queryKey, promise, configuration to tweak its behaviour)
   const { isLoading, data, isError, error, isFetching, refetch } = useSuperHeroesData(onSuccess, onError)
   
 
-  console.log({ isLoading, isFetching });
+    const {mutate: AddHero, isLoading: heroLoading, isError: heroError, error: hError} = useAddSuperHeroData()
+
+  // console.log({ isLoading, isFetching });
+
+  const handleAddHeroClick = () => {
+    console.log({name, alterEgo})
+    const hero = {name, alterEgo}
+    AddHero(hero)
+  }
+
+
+  if (heroLoading) {
+    return <h2>Hero Loading....</h2>
+  }
+  if (heroError) {
+    return <h2>{hError.message}</h2>
+  }
 
   if (isLoading || isFetching) {
     return <h2>Loading....</h2>;
@@ -37,7 +55,20 @@ const RQSuperHeroesPage = () => {
   return (
     <>
       <h2> RQ Super Heroes Page</h2>
-      {/* onclick of te button the data from api will be displayed */}
+      <div>
+        <input type="text" 
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+         />
+        <input type="text" 
+        value={alterEgo}
+        onChange={(e) => setAlterEgo(e.target.value)}
+        placeholder="alterEgo"
+         />
+         <button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
+      {/* onclick of the button the data from api will be displayed */}
       <button onClick={refetch}>Fetch Heroes</button>
       {data?.data.map((hero) => (
         <div key={hero.id}>
--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,9 +1,16 @@
-import { useQuery } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import axios from "axios";
+
+
 
 const fetchSuperHeroes = () => {
     return axios.get("http://localhost:4000/superheroes");
   };
+
+const addSuperHero = (hero) => {
+    return axios.post("http://localhost:4000/superheroes", hero)
+}
+
 export const useSuperHeroesData = (onSuccess, onError, IsQueryEnabled) => {
    return useQuery(
         "super-heroes",
@@ -48,4 +55,29 @@ export const useSuperHeroesData = (onSuccess, onError, IsQueryEnabled) => {
         
         
         })
+}
+
+// useMutation hook is used to create, update and delete a data. 
+
+export const useAddSuperHeroData = () => {
+    // in useMutaion hook we don't need to pass queryKey necassrily
+    const queryClient = useQueryClient() 
+    return useMutation(addSuperHero, {
+        onSuccess: (data) => {
+            // by doing these you don't want to again refetch by clicking Button it will automatically added as soon as addHero button is clicked.
+
+            // queryClient.invalidateQueries('super-heroes')
+
+            // setQueryData is used to update the mutate response by doing so we reducing the network request
+            queryClient.setQueryData('super-heroes', (oldQueryData) => {
+                return {
+                    ...oldQueryData,
+                    // In data : we will spread out the oldQueryData and mutation respnose that is newData --> data.data
+                    data: [...oldQueryData.data, data.data]
+                }
+            })
+
+        }
+    })
+
 }
